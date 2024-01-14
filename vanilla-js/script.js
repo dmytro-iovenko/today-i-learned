a10c433b99edaa372ae0772f7ab6c3a751566941
Dmytro Iovenko <iovenko@gmail.com>
2024-01-14 16:26:11 -0500

    fetching data from supabase table

--- vanilla-js/script.js
+++ vanilla-js/script.js
@@ -9,51 +9,31 @@ const CATEGORIES = [
   { name: "news", color: "#8b5cf6" },
 ];
 
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
-
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
 const factsList = document.querySelector(".facts-list");
 
 factsList.innerHTML = "";
 
-createFactsList(initialFacts);
+loadFacts();
+
+async function loadFacts() {
+    const res = await fetch("https://jpnjisjdtomhzxdyhcrm.supabase.co/rest/v1/facts", {
+        headers: {
+          apikey:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impwbmppc2pkdG9taHp4ZHloY3JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUyMDcxMTAsImV4cCI6MjAyMDc4MzExMH0.TKk0U1CZ3fpZuHyOpFWR_mWeesO2HC-YCv9vrKmhO_w",
+          authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impwbmppc2pkdG9taHp4ZHloY3JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUyMDcxMTAsImV4cCI6MjAyMDc4MzExMH0.TKk0U1CZ3fpZuHyOpFWR_mWeesO2HC-YCv9vrKmhO_w",
+        }
+      });
+      const data = await res.json();
+      console.log(data);
+      createFactsList(data);
+}
 
 function createFactsList(dataArray) {
   const htmlArr = dataArray.map(
-    (fact) =>  `<li class="fact">
+    (fact) => `<li class="fact">
                   <p>
                     ${fact.text}
                       <a
