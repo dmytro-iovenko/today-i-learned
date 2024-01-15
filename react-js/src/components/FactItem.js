0a9189f445b8babbe67a8b5f0ee3565207af601e
Dmytro Iovenko <iovenko@gmail.com>
2024-01-15 17:51:46 -0500

    rendering the list of facts

---
+++ react-js/src/components/FactItem.js
@@ -0,0 +1,24 @@
+function FactItem(props) {
+  const { fact } = props;
+
+  return (
+    <li className="fact">
+      <p>
+        {fact.text}
+        <a className="source" href={fact.source} target="_blank">
+          (Source)
+        </a>
+      </p>
+      <span className="tag" /*style="background-color: #3b82f6"*/>
+        {fact.category}
+      </span>
+      <div className="vote-buttons">
+        <button>👍 {fact.votesInteresting}</button>
+        <button>🤯 {fact.votesMindblowing}</button>
+        <button>⛔️ {fact.votesFalse}</button>
+      </div>
+    </li>
+  );
+}
+
+export default FactItem;
